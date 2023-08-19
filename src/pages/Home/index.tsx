958e5f41617fd993da8ee82b06280e49b3a2a78f
White Chen <white.chen@zerologix.com>
2023-08-19 22:59:09 +0800

    feat(abc-002): create the component for menu button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,74 @@
 import React from 'react'
-import Demo from 'src/pages/Demo'
+import { Text, MenuItem, Icon } from '@tonic-ui/react'
+import MenuButton from 'components/MenuButton'
 import styled from './Home.module.scss'
 
 const Home = () => {
   return (
     <div className={styled.wrapper}>
       <div className={styled.square}>
-        <Demo />
+        <MenuButton toggleIcon={<Icon icon='face-smile-o' size='8x' color='yellow:50' />}>
+          <MenuItem>
+            <Icon icon='settings' mr='2x' />
+            <Text>Settings</Text>
+          </MenuItem>
+          <MenuItem>
+            <Icon icon='user-team' mr='2x' />
+            <Text>Accounts</Text>
+          </MenuItem>
+          <MenuItem>
+            <Icon icon='lock' mr='2x' />
+            <Text>Privacy control</Text>
+          </MenuItem>
+        </MenuButton>
       </div>
       <div className={styled.square}>
-        <Demo style={{ position: 'absolute', right: 0, bottom: 0 }} />
+        <MenuButton style={{ position: 'absolute', right: 0, bottom: 0 }}>
+          <MenuItem>
+            <Icon icon='settings' mr='2x' />
+            <Text>Settings</Text>
+          </MenuItem>
+          <MenuItem>
+            <Icon icon='user-team' mr='2x' />
+            <Text>Accounts</Text>
+          </MenuItem>
+          <MenuItem>
+            <Icon icon='lock' mr='2x' />
+            <Text>Privacy control</Text>
+          </MenuItem>
+        </MenuButton>
       </div>
       <div className={styled.square}>
-        <Demo />
+        <MenuButton>
+          <MenuItem>
+            <Icon icon='settings' mr='2x' />
+            <Text>Settings</Text>
+          </MenuItem>
+          <MenuItem>
+            <Icon icon='user-team' mr='2x' />
+            <Text>Accounts</Text>
+          </MenuItem>
+          <MenuItem>
+            <Icon icon='lock' mr='2x' />
+            <Text>Privacy control</Text>
+          </MenuItem>
+        </MenuButton>
       </div>
       <div className={styled.square}>
-        <Demo />
+        <MenuButton>
+          <MenuItem>
+            <Icon icon='settings' mr='2x' />
+            <Text>Settings</Text>
+          </MenuItem>
+          <MenuItem>
+            <Icon icon='user-team' mr='2x' />
+            <Text>Accounts</Text>
+          </MenuItem>
+          <MenuItem>
+            <Icon icon='lock' mr='2x' />
+            <Text>Privacy control</Text>
+          </MenuItem>
+        </MenuButton>
       </div>
     </div>
   )
